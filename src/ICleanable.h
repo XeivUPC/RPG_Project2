1ef3ad35fd844b41150aadfe3fe7c88ab1e59fc7
Ana Alcázar Cobo <ana.alcazar@estudiantat.upc.edu>
2025-02-17 11:48:36 +0100

    reset

--- src/ICleanable.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class ICleanable {
-public:
-	virtual bool CleanUp() = 0;
-	virtual ~ICleanable() {}
-};
--- src/IRendereable.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-class IRendereable {
-public:
-	virtual void Render() = 0;
-	virtual ~IRendereable() {}
-	int renderLayer = 0;
-};
