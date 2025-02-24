e4a6fbe0de0b0b79b07f62136e935b564ce2e30b
Xeiv360 <145395703+XeivUPC@users.noreply.github.com>
2025-02-24 17:44:48 +0100

    colors

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 function setHTML(htmlAddres)
 {
     const frame = document.getElementById("frame");
+    if(frame.getAttribute("src") == "./html/"+ htmlAddres +".html"){
+        return;
+    }
     frame.setAttribute("src", "./html/"+ htmlAddres +".html");
 }
