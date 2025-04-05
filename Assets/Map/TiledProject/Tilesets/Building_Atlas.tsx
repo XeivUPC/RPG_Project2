6a005cf8b4c47c740eba68591d71892aeab01d44
M4xp0w3rg4m3s <156011390+M4xp0w3rg4m3s@users.noreply.github.com>
2025-04-05 12:51:10 +0200

    Houses initial

--- Assets/Map/TiledProject/Tilesets/Building_Atlas.tsx
+++ Assets/Map/TiledProject/Tilesets/Building_Atlas.tsx
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="building_atlas" tilewidth="96" tileheight="107" tilecount="3" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="building_atlas" tilewidth="256" tileheight="272" tilecount="6" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent1.png" width="96" height="107"/>
+  <image width="96" height="107" source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent1.png"/>
   <objectgroup draworder="index" id="2">
    <object id="1" name="renderPosition" x="48" y="95">
     <point/>
@@ -17,7 +17,7 @@
   </objectgroup>
  </tile>
  <tile id="1">
-  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent2.png" width="96" height="107"/>
+  <image width="96" height="107" source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent2.png"/>
   <objectgroup draworder="index" id="2">
    <object id="7" name="renderPosition" x="48.0057" y="93.4091">
     <point/>
@@ -32,7 +32,7 @@
   </objectgroup>
  </tile>
  <tile id="2">
-  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent3.png" width="96" height="107"/>
+  <image width="96" height="107" source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent3.png"/>
   <objectgroup draworder="index" id="2">
    <object id="7" name="renderPosition" x="47.9403" y="93.4347">
     <point/>
@@ -42,6 +42,57 @@
    <object id="10" name="collision" x="31.9403" y="46.4347" width="32" height="47.2727"/>
    <object id="11" name="entryCollider" x="32.0113" y="93.8892" width="31.9077" height="1.27273"/>
    <object id="12" name="exitPosition" x="47.9403" y="100.435">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <image width="112" height="160" source="../../../Textures/Atlas/MapObjects/Buildings/Single/House2.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="0" y="39.3333" width="111.697" height="104.333"/>
+   <object id="2" name="collision" x="16.3333" y="143.884" width="28.6667" height="15.9275"/>
+   <object id="3" name="collision" x="66.9167" y="143.88" width="28.25" height="15.8674"/>
+   <object id="4" name="entryCollider" x="45.0909" y="152.545" width="21.8182" height="3.09091"/>
+   <object id="5" name="collision" x="45.2727" y="143.818" width="21.4545" height="8.72727"/>
+   <object id="6" name="exitPosition" x="55.9091" y="159.364">
+    <point/>
+   </object>
+   <object id="7" name="renderPosition" x="55.75" y="143.375">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <image width="160" height="272" source="../../../Textures/Atlas/MapObjects/Buildings/Single/House3.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="0" y="39.8182" width="111.636" height="205.994"/>
+   <object id="2" name="collision" x="112.136" y="83" width="31.5909" height="81.8182"/>
+   <object id="3" name="collision" x="16" y="246.375" width="24" height="15"/>
+   <object id="4" name="collision" x="71.75" y="246.375" width="24" height="14.875"/>
+   <object id="6" name="entryCollider" x="40.3438" y="246.219" width="31.0625" height="3.625"/>
+   <object id="7" name="renderPosition" x="55.75" y="153.5">
+    <point/>
+   </object>
+   <object id="8" name="exitPosition" x="55.8125" y="257.574">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <image width="256" height="144" source="../../../Textures/Atlas/MapObjects/Buildings/Single/House4.png"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="renderPosition" x="127.5" y="113.75">
+    <point/>
+   </object>
+   <object id="4" name="collider" x="32.75" y="50" width="31.25" height="72.25"/>
+   <object id="5" name="collider" x="20.25" y="115.75" width="7.5" height="13"/>
+   <object id="6" name="collider" x="227.813" y="117" width="8.125" height="12.1875"/>
+   <object id="7" name="collider" x="191.25" y="50" width="32" height="71"/>
+   <object id="8" name="collider" x="64.0909" y="34.25" width="51.9091" height="104.75"/>
+   <object id="10" name="collider" x="139.795" y="34.25" width="51.9091" height="104.75"/>
+   <object id="12" name="collider" x="116.182" y="34.1818" width="23.2727" height="96"/>
+   <object id="13" name="entryCollider" x="116.063" y="130.375" width="23.5" height="4.375"/>
+   <object id="14" name="exitPosition" x="127.938" y="141">
     <point/>
    </object>
   </objectgroup>
