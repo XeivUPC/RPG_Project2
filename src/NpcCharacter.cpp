d9c35b0787594507d8c6ae95df4530ea41ddfdb6
Xeiv360 <145395703+XeivUPC@users.noreply.github.com>
2025-04-06 19:30:47 +0200

    a

--- src/NpcCharacter.cpp
+++ src/NpcCharacter.cpp
@@ -159,7 +159,7 @@ void NpcCharacter::Animate()
 	string animationId = isMoving ? (speedModifier == runSpeedModifier ? "run-" : "walk-") : "idle-";
 	string animationDirectionId = "";
 
-	if (std::abs(animationDirection.x) > 0) {
+	if (std::abs(animationDirection.x) > 0.2f) {
 
 		animationDirectionId = "horizontally";
 	}
