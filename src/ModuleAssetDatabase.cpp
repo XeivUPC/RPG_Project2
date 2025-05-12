756e197a2ae4d458569fed4678455097142c99d0
Max <m4xp0w3r.dev@gmail.com>
2025-05-12 12:56:00 +0200

    Title Background

--- src/ModuleAssetDatabase.cpp
+++ src/ModuleAssetDatabase.cpp
@@ -104,7 +104,12 @@ void ModuleAssetDatabase::LoadAssets()
 	AddTextureToStorage("member_characterUI", *textureFactory->CreateTexture("Assets/Textures/UI/memberCharacterUI.png"));
 	AddTextureToStorage("dialogue_answerBox", *textureFactory->CreateTexture("Assets/Textures/UI/dialogue_answerBox.png"));
 	AddTextureToStorage("compass_overlay", *textureFactory->CreateTexture("Assets/Textures/UI/compass_overlay.png"));
-	AddTextureToStorage("compass_arrow", *textureFactory->CreateTexture("Assets/Textures/UI/compass_arrow.png"));
+	AddTextureToStorage("title_bg1", *textureFactory->CreateTexture("Assets/Textures/Parallax/TitleBG/title_bg1.png"));
+	AddTextureToStorage("title_bg2", *textureFactory->CreateTexture("Assets/Textures/Parallax/TitleBG/title_bg2.png"));
+	AddTextureToStorage("title_bg3", *textureFactory->CreateTexture("Assets/Textures/Parallax/TitleBG/title_bg3.png"));
+	AddTextureToStorage("title_bg4", *textureFactory->CreateTexture("Assets/Textures/Parallax/TitleBG/title_bg4.png"));
+	AddTextureToStorage("title_bg5", *textureFactory->CreateTexture("Assets/Textures/Parallax/TitleBG/title_bg5.png"));
+	AddTextureToStorage("title_bg6", *textureFactory->CreateTexture("Assets/Textures/Parallax/TitleBG/title_bg6.png"));
 	///Cursors
 	AddTextureToStorage("mouse_cursor0", *textureFactory->CreateTexture("Assets/Textures/Cursors/mouse_cursor_0.png"));
 	AddTextureToStorage("mouse_cursor1", *textureFactory->CreateTexture("Assets/Textures/Cursors/mouse_cursor_1.png"));
