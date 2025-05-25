4faa6d6583e3121234ab4213d9125f8f0472a433
AnaAlcazarCobo <ana.alcazar@estudiantat.upc.edu>
2025-05-25 23:54:18 +0200

    a

---
+++ Assets/Map/TiledProject/Tilesets/buildings.tsx
@@ -0,0 +1,276 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="buildings" tilewidth="96" tileheight="96" tilecount="16" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_blue_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_blue_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="2" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="3" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="4" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="5" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="6" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="7" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_brown_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_brown_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="2" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="3" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="4" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="5" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="6" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="7" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_green_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_green_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="2" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="3" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="4" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="5" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="6" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="7" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_white_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_white_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="2" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="3" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="4" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="5" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="6" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="7" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_yellow_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_yellow_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="2" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="3" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="4" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="5" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="6" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="7" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_pink_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_pink_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="3">
+   <object id="8" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="9" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="10" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="11" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="12" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="13" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="14" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_purple_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="47" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="13" type="renderPosition">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_purple_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="3">
+   <object id="4" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="5" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="6" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="7" name="collision" x="59" y="86.9995" width="36" height="7.00051"/>
+   <object id="8" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="9" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="10" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_red_closed.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="88" width="92" height="6"/>
+   <object id="2" name="collision" x="2" y="46.75" width="92" height="6"/>
+   <object id="3" name="collision" x="9" y="47" width="78" height="47"/>
+   <object id="4" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="TargetPath" value=""/>
+   <property name="Type" value="building"/>
+  </properties>
+  <image source="../../../Textures/Atlas/MapObjects/Buildings/Single/tent_red_opened.png" width="96" height="96"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" name="collision" x="2" y="47" width="92" height="8"/>
+   <object id="2" name="collision" x="9" y="47" width="78" height="40"/>
+   <object id="3" name="collision" x="1" y="87" width="35" height="7"/>
+   <object id="4" name="collision" x="59" y="87" width="36" height="7"/>
+   <object id="5" name="entryCollider" x="36" y="88" width="23" height="2"/>
+   <object id="8" name="renderPosition" x="48" y="50">
+    <point/>
+   </object>
+   <object id="9" name="exitPosition" x="48" y="92">
+    <point/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
