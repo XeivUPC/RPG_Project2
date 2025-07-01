e3dcc38d2f102cc2d9cfded33c57919afc402fab
JoseTurdeZarandieta <160478852+JoseTurdeZarandieta@users.noreply.github.com>
2025-07-01 19:57:43 +0200

    .

---
+++ js/games.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const gameImage = document.getElementById("game-download-image");
+
+    gameImage.addEventListener("click", function () {
+        const filePath = "../assets/game_build.zip";
+        const filename = filePath.replace(/^.*[\\/]/, '');
+
+        axios({
+            url: filePath,
+            method: 'GET',
+            responseType: 'blob'
+        })
+        .then((response) => {
+            if (response.status === 200) {
+                const url = window.URL.createObjectURL(new Blob([response.data]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', filename);
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                window.URL.revokeObjectURL(url);
+            } else {
+                console.error('Error: Received non-200 status code');
+            }
+        })
+        .catch((error) => {
+            console.error('Error downloading the game:', error);
+        });
+    });
+});
